e50f748d68ae4e5d3fdb119559d0b398c014670d
Thiago <114317304+t-golv@users.noreply.github.com>
2024-01-30 04:50:35 -0300

    new commit

--- script/script.js
+++ script/script.js
@@ -1,17 +1,17 @@
 const upload = document.getElementById("file-upload");
+let footer = document.querySelector(".footer");
+let recentContainer = document.querySelector(".recent-container");
 
-upload.addEventListener("change", (e) => {
-  const fileName = e.target.files[0].name;
-  // console.log(e.target);
-  console.log(Date.now());
-  saveToLocal(fileName, e.target.files[0].lastModified);
-});
+function refreshDate() {
+  let el = document.getElementById("date-foot");
+  el.innerText = new Date().toLocaleString();
+  footer.appendChild(el);
+}
 function getRecentFromStorage() {
   let getStorage = window.localStorage.getItem("recent-files");
-  let recentContainer = document.querySelector(".recent-container");
   if (getStorage && getStorage != "[]") {
     recentContainer.innerHTML = `
-    <div class="item recent-header">
+    <div class=" recent-header">
     <h3>
       <div class="icon"><img src="assets/file.svg" alt=""></div>
       Name:</h3>
@@ -23,7 +23,7 @@ function getRecentFromStorage() {
       item.innerHTML = `
       <h3>
       <img src="assets/txt.svg" alt="">
-      ${el.filename}
+      <strong >${el.filename}</strong>
       </h3>
       <h3>${new Date(el.date).toLocaleString({
         year: "numeric",
@@ -33,14 +33,19 @@ function getRecentFromStorage() {
         minute: "numeric",
         second: "2-digit",
       })}</h3>`;
+      item.addEventListener("click", (e) => {
+        window.localStorage.setItem("open-file", JSON.stringify(el));
+        window.location.href = "../pages/app.html";
+      });
       recentContainer.appendChild(item);
     });
   } else {
     recentContainer.innerHTML = `
-    <div class="item recent-header">
+    <div class=" recent-header">
           <h3>
             <div class="icon"><img src="assets/file.svg" alt=""></div>
-            Name:</h3>
+            Name:
+          </h3>
           <h3>Last time:</h3>
     </div>
     <div class="item">
@@ -50,18 +55,22 @@ function getRecentFromStorage() {
     `;
   }
 }
-getRecentFromStorage();
-
-function saveToLocal(fileName, time) {
+function saveToLocal(fileName, time, content = "") {
   let getStorage = window.localStorage.getItem("recent-files");
-  let newRecentFiles = { filename: fileName, date: time };
-  if (getStorage) {
+  let newRecentFiles = { filename: fileName, date: time, content: content };
+  if (getStorage && JSON.parse(getStorage).length < 8) {
     let storage = JSON.parse(getStorage);
-    // console.log(storage);
     storage = storage.filter((el) => {
       return el.filename != newRecentFiles.filename;
     });
-    console.log("storage: ", storage);
+    storage.unshift(newRecentFiles);
+    window.localStorage.setItem("recent-files", JSON.stringify(storage));
+  } else if (getStorage && JSON.parse(getStorage).length >= 8) {
+    let storage = JSON.parse(getStorage);
+    storage = storage.filter((el) => {
+      return el.filename != newRecentFiles.filename;
+    });
+    storage.pop();
     storage.unshift(newRecentFiles);
     window.localStorage.setItem("recent-files", JSON.stringify(storage));
   } else {
@@ -70,4 +79,33 @@ function saveToLocal(fileName, time) {
     window.localStorage.setItem("recent-files", JSON.stringify(getStorage));
   }
   // window.location.reload();
+}
+function loadOpened() {
+  let fileToOpen = window.localStorage.getItem("open-file");
+  if (fileToOpen) {
+    let headerText = document.querySelector(".main-text");
+    fileToOpen = JSON.parse(fileToOpen);
+    document.title = fileToOpen.filename.slice(0, -4).toUpperCase();
+    headerText.innerText = fileToOpen.filename.slice(0, -4).toUpperCase();
+  }
+}
+
+if (document.title == "Marked Word") {
+  upload.addEventListener("change", (e) => {
+    const fileName = e.target.files[0].name;
+    if (e.target.files[0].size < 10000) {
+      let fr = new FileReader();
+      fr.onload = function () {
+        saveToLocal(fileName, e.target.files[0].lastModified, fr.result);
+        getRecentFromStorage();
+      };
+      console.log(fr.readAsText(e.target.files[0]));
+    } else {
+      alert("File has to be less than 10kb");
+    }
+  });
+  getRecentFromStorage();
+  refreshDate();
+} else {
+  loadOpened();
 }
